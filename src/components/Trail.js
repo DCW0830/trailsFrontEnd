b1afc197ef62aa88b2c7cca99cd120017c19ab8d
github username <github email address>
2019-01-15 10:06:02 -0600

    hello

--- src/components/Trail.js
+++ src/components/Trail.js
@@ -1,36 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Trail extends Component {
+const Trail = (props) => {
 
-  state = {
-    trail: []
-  }
+  const {name, difficulty, length, location, stars, id} = props.trail
 
-  handleClick = (event, trailId) => {
-    this.state.trail.filter(trail => {
-      if(trail !== trailId) {
-        return this.setState ({
-          trail: [...this.state.trail, trailId]
-        })
-      } else {
-        return trail
-      }
-    })
-
-  }
-  render() {
-  const {name, difficulty, length, location, stars} = this.props.trail
-    return (
-      <tr>
-        <td>{name}</td>
-        <td>{difficulty}</td>
-        <td>{length}</td>
-        <td>{location}</td>
-        <td>{stars}</td>
-        <td><input onChange={(event)=> this.handleClick(event, this.props.trail.id)} type ='checkbox'/></td>
-      </tr>
-    )
-  }
+  return (
+    <tr onClick={()=> console.log} className="song">
+      <td>{name}</td>
+      <td>{difficulty}</td>
+      <td>{length}</td>
+      <td>{location}</td>
+      <td>{stars}</td>
+      <td>
+        <input
+          onChange={(event)=>props.handleClick(event, id)}
+          type ='checkbox'
+        />
+      </td>
+    </tr>
+  )
 }
 
 export default Trail;
--- src/containers/SearchedTrailList.js
+++ src/containers/SearchedTrailList.js
@@ -3,11 +3,31 @@ import {connect} from 'react-redux'
 import Trail from '../components/Trail'
 
 class SearchedTrailList extends Component {
+  state = {
+    selectedTrails: []
+  }
+
+  handleClick = (event, trailId) => {
+    if(event.target.checked) {
+      this.setState({
+        selectedTrails: [...this.state.selectedTrails, trailId]
+      }, ()=>console.log(this.state.selectedTrails))
+      console.log('hi')
+    } else {
+      this.setState({
+        selectedTrails: this.state.selectedTrails.splice( this.state.selectedTrails.indexOf(trailId), 1 )
+      }, ()=> console.log(this.state.selectedTrails))
+    }
+  }
 
   createTrail = () => {
     if (this.props.trails.trails) {
       return this.props.trails.trails.map(trailObj => {
-        return <Trail key={trailObj.id} trail={trailObj} />
+        return <Trail
+          handleClick={this.handleClick}
+          key={trailObj.id}
+          trail={trailObj}
+        />
       })
     }
   }
